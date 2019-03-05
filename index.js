308d80f65b237a67b022ec389db29cd7805f768e
DanielArmengod <danielarmengodsorrosal@gmail.com>
2019-03-05 11:16:47 +0100

    Corrección de los errores principales al lanzar el entrenador de datos, implementación de scanner donde el usuario puede preguntar al bot vía consola

--- index.js
+++ index.js
@@ -8,14 +8,10 @@ const config = {
 	leakyReluAlpha: 0.01
 }
 
-var rl = readLine.createInterface({
-	input : process.stdin,
-	output: process.stdout
-});
+var miBrain = new brain.recurrent.LSTM();
 
-const miBrain = new brain.recurrent.LSTM;
-
-miBrain.train = ([
+// entrenamos a la máquina para que recoga las entradas y salidas que debe de aprender
+miBrain.train([
 	{input : 'Hola buenas', output : 'Hola buenas, ¿Que tal estas?'},
 	{input : '¿Como te llamas?', output : 'Mi nombre es BotChat, encantado de conocerte.'},
 	{input : 'Cuentame algo', output : 'No se que contarte, ¿que es lo que mas te gusta hacer en tus ratos libres?'},
@@ -23,20 +19,21 @@ miBrain.train = ([
 	{input : 'Me encuentro mal', output : 'Si te encuentras mal, ves al médico'},
 	{input : 'Estoy contento y alegre', output : 'Me encanta, yo tambien estoy bién'},
 	{input : 'Necesito ayuda', output : 'Soy un chat robot, me puedes preguntar todo lo que quieras, así que adelante.'},
-	{input : '', output : 'No te entiendo, me puedes decir lo que quieres'},
 ]);
 
+// instanciamos el buffer de entrada de texto, para que el usuario pueda introducir un mensaje
 var rl = readLine.createInterface({
 	input : process.stdin,
 	output: process.stdout
 });
 
 
+// se lanza la escucha de el teclado de entrada para recoger la respuesta del usuario
 rl.question('Pregunta: ("s" para salir) ', (answer) => {
-	
+	console.log('Pregunta: ' + answer);
+
 	const respuesta = miBrain.run(answer);
-	console.log('Respuesta: ${respuesta}');
+	console.log('Respuesta: ' + respuesta);
 
 	rl.close();
 });
-
